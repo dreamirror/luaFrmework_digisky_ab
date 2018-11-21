fbd39e24e7954edd772197b4b1aa26e07b644123
zhourui@digisky.com <zhourui@digisky.com>
2018-11-21 18:25:25 +0800

    抽象lua对abMgr的使用，准备lau与C#的交互，修改异步加载回调为lamada

---
+++ DigiSkyAssetBundle/Script/AbMgr.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using LuaFramework;
+namespace DigiSky.AssetBundleKit
+{
+    class AbMgr:Manager
+    {
+        /// <summary>
+        /// 加载ab
+        /// </summary>
+        /// <param path="assetbudlePath">ab的路径</param>
+        public void LoadAssetBundle(string path,string luaCall, params object[] args) {
+            AssetBundleManager.GetSingel().LoadAssetAsyc("Assets/StreamingAssets/assetbundles/pic", "1", (UnityEngine.Object obj) => {
+                LuaManager.CallFunction(luaCall, args);
+            });
+        }
+
+        /// <summary>
+        /// 加载asset
+        /// </summary>
+        /// <param assetName="assetName"></param>
+        public void LoadAsset(string assetName)
+        {
+
+        }
+        /// <summary>
+        /// 加载asset
+        /// </summary>
+        /// <param path="assetbudlePath">ab的路径</param>
+        /// <param assetName="assetName"></param>
+        public void LoadAsset(string path,string assetName)
+        {
+
+        }
+
+        /// <summary>
+        /// 卸载AB
+        /// </summary>
+        /// <param path="assetbudlePath">ab的路径</param>
+        /// <param assetName="assetName"></param>
+        public void UnLoadAssetBundle(string path)
+        {
+
+        }
+
+        /// <summary>
+        /// 卸载AB
+        /// </summary>
+        /// <param ab="assetbudle">ab的路径</param>
+        /// <param assetName="assetName"></param>
+        public void UnLoadAssetBundle(AssetBundle ab)
+        {
+            ab.Unload(false); //非暴力卸载
+        }
+    }
+}
